2181d9ae464a7eec42c7746b363713bbc9c36542
Joseph Camacho <spongkj@gmail.com>
2023-10-30 05:46:50 +0800

    ok

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
-function logMeIn(u,p)
-   {  
-      var uid = document.getElementById(u).value;
-      var pass = document.getElementById(p).value;
-      var pad = "Database1.accdb";
-      var cn = new ActiveXObject("ADODB.Connection");
-      var connStr = "Provider=microsoft.ace.oledb.12.0;Data Source=" + pad;
-      cn.Open(connStr);
-      var rs = new ActiveXObject("ADODB.Recordset");
-      var sql = "SELECT UserID, Password, FirstName FROM Login WHERE UserID = '" + uid + "' AND Password = '" + pass + "';";
-      rs.Open(sql, cn);
-      if(!rs.bof)
-      {
-         window.alert("Hello " + rs.fields(2).value + "!");
-      }
-      else
-      {  
-         window.alert("Invalid user or password");
-      }
-      rs.Close();
-      cn.Close();
-   }
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("login-form");
+    const message = document.getElementById("message");
 
-function loginOnKeyPress(e)
-   {  
-      if(e && e.keyCode == 13)
-         {  
-            logMeIn();
-         }
-   }
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+
+        // Make an AJAX request to check login
+        const xhr = new XMLHttpRequest();
+        xhr.open("POST", "check-login.php", true);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                const response = JSON.parse(xhr.responseText);
+                if (response.success) {
+                    message.textContent = "Login successful!";
+                } else {
+                    message.textContent = "Login failed. Please check your username and password.";
+                }
+            }
+        };
+
+        const data = `username=${username}&password=${password}`;
+        xhr.send(data);
+    });
+});
